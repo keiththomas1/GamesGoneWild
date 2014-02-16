366dc76b65a522111eaeac31c70907c911fa349b
Keith Thomas <kryzodoze@gmail.com>
2014-02-16 16:56:40 -0600

    Working version of the beer pong minigame. Also working on Android.

--- Assets/Scripts/BeerPongController.cs
+++ Assets/Scripts/BeerPongController.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class BeerPongController : MonoBehaviour 
 {
 	bool isShooting;
+	bool isFinished;
+
+	public GameObject ballParent;
 	public GameObject ball;
 	Vector3 initialBallSize;
-	Vector2 upBallMovement;
+	Vector2 ballMovement;
 	Vector2 downBallMovement;
 	float ballShrinkRate = .98f;
 	
@@ -17,16 +20,27 @@ public class BeerPongController : MonoBehaviour
 	Vector2 rightSlide;
 	Vector2 leftSlide;
 
+	public GameObject cup;
+	public GameObject goodJobText;
+
 	// Use this for initialization
 	void Start () 
 	{
 		isShooting = true;
-		initialBallSize = ball.transform.localScale;
+		isFinished = false;
+		goodJobText.renderer.enabled = false;
 
-		slideBarActualLength = slideBar.renderer.bounds.size.x * .9f;
+		initialBallSize = ball.transform.localScale;
+		ballMovement = new Vector2( 0.0f, .1f );
+
+
+		slideBarActualLength = slideBar.renderer.bounds.size.x * .9f; // HACK - This is just because the bar is curved.
 		sliderDirection = "right";
 		rightSlide = new Vector2( .1f, 0.0f );
 		leftSlide = new Vector2( -.1f, 0.0f );
+
+		ball.GetComponent<Animator>().enabled = false;
+		//ball.GetComponentInChildren<Animator>().enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -36,7 +50,7 @@ public class BeerPongController : MonoBehaviour
 		{
 			if( "right" == sliderDirection )
 			{
-				ball.transform.Translate( rightSlide );
+				ballParent.transform.Translate( rightSlide );
 				slider.transform.Translate( rightSlide );
 
 				if( slider.transform.position.x >= (slideBar.transform.position.x + slideBarActualLength/2) )
@@ -46,7 +60,7 @@ public class BeerPongController : MonoBehaviour
 			}
 			else // if "left" == sliderDirection
 			{
-				ball.transform.Translate( leftSlide );
+				ballParent.transform.Translate( leftSlide );
 				slider.transform.Translate( leftSlide );
 				
 				if( slider.transform.position.x <= (slideBar.transform.position.x - slideBarActualLength/2) )
@@ -57,25 +71,43 @@ public class BeerPongController : MonoBehaviour
 
 			if( Input.GetMouseButtonDown( 0 ) )
 			{
-				float xDifferential = (slider.transform.position.x - slideBar.transform.position.x) * .01f;
-				upBallMovement = new Vector2( xDifferential, .1f );
-				downBallMovement = new Vector2( xDifferential, -.1f );
-
 				isShooting = false;
+				ball.GetComponent<Animator>().enabled = true;
 			}
 		}
-		else  // If ball is in air 
+		else // If ball is in air
+		{
+			if( ball.transform.position.y > 4.0f ) // HACK - This just happens to be because current set-up
+			{
+				if( Mathf.Abs(slider.transform.position.x - cup.transform.position.x) < .4 )
+				{
+					goodJobText.renderer.enabled = true;
+				}
+
+				ball.GetComponent<Animator>().enabled = false;
+				isFinished = true;
+			}
+		}
+
+		if( isFinished )
+		{
+			if( Input.GetMouseButtonDown( 0 ) )
+			{
+				Application.LoadLevel( 0 );
+			}
+		}
+		/*else  // If ball is in air 
 		{
 			if( ball.transform.localScale.x > initialBallSize.x/2 )
 			{
-				ball.transform.Translate( upBallMovement );
+				ball.transform.Translate( ballMovement );
 				ball.transform.localScale = ball.transform.localScale * ballShrinkRate;
 			}
 			else
 			{
-				ball.transform.Translate( downBallMovement );
+				ball.transform.Translate( ballMovement );
 				ball.transform.localScale = ball.transform.localScale * ballShrinkRate;
 			}
-		}
+		}*/
 	}
 }
