2fc12647b379a4832e71e0fa47cb4204dc36a793
Raul G Luna Jr <jluna8@uic.edu>
2014-02-28 14:12:54 -0600

    Save The Floors Scenes along with any assets and scripts. Currently destroys puke when caught in bucket. Bucket moves horizontal only.

---
+++ Assets/Scripts/Bucket_Controller.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* With this script, we would like to control the garbage bucket with a click and drag movement
+ * when using a mouse and a swipe in our mobile platforms
+ */
+public class Bucket_Controller : MonoBehaviour {
+	
+	// Garbage bucket that will catch the puke stream
+	GameObject Bucket;
+	
+	// Ideally, garbage bucket speed
+	public float speed = 0.1f;
+	
+	// For keyboard
+	Vector3 screenPoint;
+	
+	// Use this for initialization
+	void Start (){
+		
+		if (Bucket == null) {
+			Bucket = GameObject.FindGameObjectWithTag ("GameController");
+			Debug.Log("GameController found\n");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//OnGUI ();
+		OnMouseOver ();
+		touchPosition ();
+	}
+	
+	void OnMouseOver()
+	{
+		
+		// This was for testing purposes
+		if (Input.GetMouseButtonDown (0)) {
+			
+			Debug.Log ("Mouse-Clicked\n");
+			//renderer.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+			screenPoint = Camera.main.WorldToScreenPoint(Bucket.transform.position);
+			OnMouseDrag ();
+		}
+	}
+	
+	void OnMouseDrag()
+	{
+		
+		// Determines screen point position in desktop mode
+		Vector3 currentScreenPoint = new Vector3 (Input.mousePosition.x, screenPoint.y, screenPoint.z);
+		Vector3 currentPosition = Camera.main.ScreenToWorldPoint (currentScreenPoint);
+		transform.position = currentPosition;
+	}
+	
+	/*void OnGUI() {
+		Event e = Event.current;
+		Debug.Log(e.mousePosition);
+	}*/
+	
+	void touchPosition()
+	{
+		
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			
+			// Determines position in mobile platform mode
+			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+			transform.Translate (-touchDeltaPosition.x * speed, 0, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/Puke_Behavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puke_Behavior : MonoBehaviour {
+
+	// Public variable
+	public float speed = 0.1f;
+
+	// 
+	GameObject Puke;
+
+	// Use this for initialization
+	void Start () {
+
+		if (Puke == null) {
+			Puke = GameObject.FindGameObjectWithTag ("Puke");
+			Debug.Log("Puke object found\n");
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D( Collider2D coll)
+	{
+		Destroy ( gameObject);
+	}
+}
