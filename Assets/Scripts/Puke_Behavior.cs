75136211823340093422c69350652aeda297953f
Raul G Luna Jr <jluna8@uic.edu>
2014-03-13 02:54:50 -0500

    Save The Floor Scene with no graphics. Is functional but not entirely done

--- Assets/Scripts/Puke_Behavior.cs
+++ Assets/Scripts/Puke_Behavior.cs
@@ -3,28 +3,48 @@ using System.Collections;
 
 public class Puke_Behavior : MonoBehaviour {
 
-	// Public variable
-	public float speed = 0.1f;
+	// Identifier for our gameobject
+	public GameObject pukePrefab;
 
-	// 
-	GameObject Puke;
+	//
+	public GameObject controller;
+
+	// For storing position of PukePrefab object
+	Vector3 pukePrefabPos;
 
 	// Use this for initialization
-	void Start () {
+	void Start (){
 
-		if (Puke == null) {
-			Puke = GameObject.FindGameObjectWithTag ("Puke");
-			Debug.Log("Puke object found\n");
-		}
+		// Finding our gameobject with the puke tag
+		pukePrefab = GameObject.FindGameObjectWithTag ("Puke");
+
+		// Storing PukePrefab gameobject (float) position
+		pukePrefabPos = transform.position;
+
+		// Debugging purposes
+		Debug.Log("Puke Position: " + pukePrefabPos);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update (){
+
 	}
 
-	void OnTriggerEnter2D( Collider2D coll)
-	{
-		Destroy ( gameObject);
+	/// <summary>
+	/// Gravity this instance. Giving the puke gameobject some gravity so it call fall
+	/// </summary>
+	public void Gravity(){
+		
+		// We need to give our gameobject some gravity inorder for it to fall.
+		// Currently, gravity is set to 0 on all pukePrefab gameobjects.
+		Debug.Log ("In Puke_Behavior.cs gravity()");
+		this.rigidbody2D.gravityScale = 1;
+	}
+
+	// Obviously destroying our puke gameobject once it collides with the bucket 
+	void OnTriggerEnter2D( Collider2D coll){
+
+		// Lets destroy the puke gameobject in the Controller file
+		controller.GetComponent<SaveTheFloorsContoller> ().DestroyPuke ();
 	}
 }
