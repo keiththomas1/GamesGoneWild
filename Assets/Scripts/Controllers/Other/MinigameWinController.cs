c8c28df99e30bad2ac82a791052d7042c15558c2
Keith Thomas <kryzodoze@gmail.com>
2014-07-06 16:08:10 -0500

    Added ability to click through the splash screen. Stretched menu screen out to fit my Android. Fixed size of points box for flippy cup. Disabled pause button during some screens.
    Also, my last commit changed some android manifests, specifically lowering the "minsdklevel",
    so we may need to look into that going further. It may also be why google games isn't working yet.

--- Assets/Scripts/Controllers/Other/MinigameWinController.cs
+++ Assets/Scripts/Controllers/Other/MinigameWinController.cs
@@ -32,8 +32,9 @@ public class MinigameWinController : MonoBehaviour
 		if( globalController.GetComponent<GlobalController>().previousMode == "FlippyCup" )
 		{ // If in flippy cup, we need to rotate the points box to face the screen.
 			Debug.Log( "Special case points box." );
-			pointsBox.transform.Rotate( new Vector3( 25.0f, 0.0f, 0.0f ) );
-			pointsBox.transform.Translate( new Vector3( 0.0f, 2.38f, 0.0f ) );
+			pointsBox.transform.Rotate( new Vector3( 35.0f, 0.0f, 0.0f ) );
+			pointsBox.transform.Translate( new Vector3( -1.5f, 2.38f, 0.0f ) );
+			pointsBox.transform.localScale = pointsBox.transform.localScale * .8f;
 		}
 
 		oldPoints = globalController.GetComponent<GlobalController>().oldPartyPoints;
